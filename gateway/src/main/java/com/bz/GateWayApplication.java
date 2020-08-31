b779b5cedccd1555cd8b204918cc1d23d21f3167
liusong <1141185877@qq.com>
2020-08-31 09:22:44 +0800

    增加gateway网管

---
+++ gateway/src/main/java/com/bz/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.bz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:11411
+ * @date: 2020/8/28 13:50
+ **/
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+}
